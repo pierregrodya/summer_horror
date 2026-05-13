aec193c42ece50918b51e032fb04088ab41c45f1
pierregrodya <grodyap88@gmail.com>
2026-05-13 10:58:24 -0400

    Complétion

--- R/LLM.R
+++ R/LLM.R
@@ -5,6 +5,8 @@ library(dplyr)
 library(stringr)
 
 input <- readRDS("data/raw/data_wiki_raw.rds")
+output_file <- "data/clean/data_wiki_clean.rds"
+batch_size <- 10
 
 input <- input |>
   mutate(type = NA)
@@ -16,9 +18,42 @@ if(ncol(input) == 7) {
     select(-Réf)
 }
 
+api_key <- Sys.getenv("GROQ_API_KEY")
+
+if (!file.exists(output_file)){
+  df <- readRDS(input)
+} else {
+  df <- readRDS(output_file)
+}
 
 
+prompt_text <- "You are a classifier. Use one word between religion and other to categorize each film based on their name"
+
+chat <- ellmer::chat_groq(
+  system_prompt = prompt_text,
+  model = "llama-3.3-70b-versatile",
+  api_key = api_key
+)
 
 
+for (i in nrow(input)){
+  res <- chat$chat(input$Titre)
+}
 
+texte <- as.character(res)
+categorization <- str_extract_all(texte, "\\w+")
+categorization
+
+z <- data.frame(categorization)
+colnames(z) <- "blabla"
+
+z <- z |> 
+  filter(blabla == "Religion" | blabla == "Other")
+
+colnames(z) <- "type"
+
+output <- input |> 
+  mutate(z)
+
+table(output$type)
 
